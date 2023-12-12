10c0d9c54314e2d66bccbdaa0c98d394e1f5a38f
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2023-12-11 20:24:21 -0500

    added survey model

---
+++ Frontend/src/app/model/survey.ts
@@ -0,0 +1,9 @@
+export class Survey {
+  name: string;
+  description: string;
+  isPublic: boolean;
+  participants: string;
+  deadline: string;
+  questions: string;
+  published: boolean = false;
+}
