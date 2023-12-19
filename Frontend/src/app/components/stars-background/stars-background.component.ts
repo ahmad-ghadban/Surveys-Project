068f3760bc97b5289abc595aa3b9cd05fd467fc0
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2023-12-18 23:03:58 -0500

    Create stars-background.component.ts

---
+++ Frontend/src/app/components/stars-background/stars-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stars-background',
+  templateUrl: './stars-background.component.html',
+  styleUrls: ['./stars-background.component.css']
+})
+export class StarsBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
