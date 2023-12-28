f6b80defcc38af09fba463a6b5bf9630b22290f9
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2023-12-28 00:36:43 -0500

    Create DataContext.cs

---
+++ WebAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Result> Results { get; set; }
+
+        public DbSet<Survey> Surveys { get; set; }
+
+    }
+}
