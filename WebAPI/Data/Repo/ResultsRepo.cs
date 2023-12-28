6a7ac469493dece4f9f7591aad67d595e7402c9e
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2023-12-28 00:36:45 -0500

    Create ResultsRepo.cs

---
+++ WebAPI/Data/Repo/ResultsRepo.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Dtos;
+using WebAPI.Interfaces;
+using WebAPI.Models;
+
+namespace WebAPI.Data.Repo
+{
+    public class ResultsRepo : IResultsRepo
+    {
+        private readonly DataContext dc;
+
+        public ResultsRepo(DataContext dc)
+        {
+            this.dc = dc;
+        }
+
+        public void addResult(AddResultDto addResultDto)
+        {
+            Result result = new Result();
+            result.userID = addResultDto.userId;
+            result.surveyID = addResultDto.surveyId;
+            result.responses = addResultDto.responses;
+
+            dc.Results.Add(result);
+        }
+
+        public async Task<IEnumerable<Result>> GetResultsAsync(int id)
+        {
+            return await dc.Results.Where(x => x.surveyID == id).ToListAsync();
+
+        }
+
+    }
+}
