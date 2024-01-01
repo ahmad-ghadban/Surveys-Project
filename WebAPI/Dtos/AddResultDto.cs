caa46e587d96367f61cd1d4cd7f6230e2ff86900
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2023-12-31 19:18:29 -0500

    Create AddResultDto.cs

---
+++ WebAPI/Dtos/AddResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Dtos
+{
+    public class AddResultDto
+    {
+        public int userId { get; set; }
+        public int surveyId { get; set; }
+        public string responses { get; set; }
+    }
+}
