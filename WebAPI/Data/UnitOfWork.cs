6e08a94ed36c8c3e8721a796519e56c497c921bb
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2023-12-31 19:18:27 -0500

    Create UnitOfWork.cs

---
+++ WebAPI/Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using WebAPI.Data.Repo;
+using WebAPI.Interfaces;
+
+namespace WebAPI.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext dc;
+
+        public UnitOfWork(DataContext dc)
+        {
+            this.dc = dc;
+        }
+
+        public IUserRepository UserRepository => 
+            new UserRepository(dc);
+
+        public IResultsRepo ResultsRepo => 
+            new ResultsRepo(dc);
+
+        public ISurveyRepository SurveyRepository => 
+            new SurveyRepository(dc);
+
+        public async Task<bool> SaveAsync()
+        {
+            return await dc.SaveChangesAsync() > 0;
+        }
+    }
+}
