a6a74d37b6ea66a590f5282b2d1a46608dbf3ba9
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-02 23:15:27 -0500

    Create AddSurveyDto.cs

---
+++ WebAPI/Dtos/AddSurveyDto.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAPI.Dtos
+{
+    public class AddSurveyDto
+    {
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public string questions { get; set; }
+
+        public string participants { get; set; }
+
+        public string deadline { get; set; }
+
+        public bool published { get; set; }
+
+        public bool isPublic { get; set; }
+    }
+}
