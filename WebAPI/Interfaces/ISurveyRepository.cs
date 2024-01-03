fe08efdd811f04f551dcd902592dd8abe47fb9e2
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-03 00:01:20 -0500

    Create ISurveyRepository.cs

---
+++ WebAPI/Interfaces/ISurveyRepository.cs
@@ -0,0 +1,16 @@
+﻿using WebAPI.Dtos;
+using WebAPI.Models;
+
+namespace WebAPI.Interfaces
+{
+    public interface ISurveyRepository
+    {
+        Task<IEnumerable<Survey>> GetSurveysAsync();
+        Task<Survey> FindID(int id);
+        void addSurvey(AddSurveyDto addSurveyDto);
+        Task<bool> SurveyAlreadyExists(string name);
+        Task<Survey> Find(string name);
+        Task<bool> SurveyDoesNotExist(int id);
+        void deleteSurvey(Survey survey);
+    }
+}
