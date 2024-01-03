dba6d58583caee7d02117b824bf2a0423facb6b1
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-03 00:01:21 -0500

    Create IUnitOfWork.cs

---
+++ WebAPI/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Interfaces
+{
+    public interface IUnitOfWork
+    {
+
+        IUserRepository UserRepository { get; }
+        IResultsRepo ResultsRepo { get; }
+
+        ISurveyRepository SurveyRepository { get; }
+        Task<bool> SaveAsync();
+    }
+}
