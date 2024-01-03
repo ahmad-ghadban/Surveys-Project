1ddf72850b4b8c0e97230bc92911f671976eff10
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-02 23:15:34 -0500

    Create IResultsRepo.cs

---
+++ WebAPI/Interfaces/IResultsRepo.cs
@@ -0,0 +1,12 @@
+﻿using WebAPI.Dtos;
+using WebAPI.Models;
+
+namespace WebAPI.Interfaces
+{
+    public interface IResultsRepo
+    {
+
+        Task<IEnumerable<Result>> GetResultsAsync(int id);
+        void addResult(AddResultDto addResultDto);
+    }
+}
