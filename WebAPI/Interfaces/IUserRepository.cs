47831cc13ba83169b05abb5be1474200ca2142c4
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-03 00:01:23 -0500

    Create IUserRepository.cs

---
+++ WebAPI/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using WebAPI.Models;
+
+namespace WebAPI.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> Authenticate(string name, string password);
+
+        Task<IEnumerable<User>> GetUsersAsync();
+
+        void Register(string name, string password);
+
+        Task<bool> UserAlreadyExists(string name);
+
+        Task<User> Find(string name);
+
+        Task<User> FindID(int id);
+    }
+}
