23e75f44e3a7fff9f2db72dcd56bd01a0e09071d
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-04 12:08:06 -0500

    Create 20230216203259_AddResultsTable.cs

---
+++ WebAPI/Migrations/20230216203259_AddResultsTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResultsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    userID = table.Column<int>(type: "int", nullable: false),
+                    surveyID = table.Column<int>(type: "int", nullable: false),
+                    questions = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    answers = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Results");
+        }
+    }
+}
