c4e7f2ec18b4992f1b3d152eb686a9b1aac1bc0b
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-04 12:08:01 -0500

    Create 20230213173058_UserActive.cs

---
+++ WebAPI/Migrations/20230213173058_UserActive.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Active",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Users");
+        }
+    }
+}
