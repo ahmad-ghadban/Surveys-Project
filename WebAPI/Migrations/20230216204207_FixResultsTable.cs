fb84393ebcc8c833911ace4e30166843adaf2180
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-05 01:35:25 -0500

    Create 20230216204207_FixResultsTable.cs

---
+++ WebAPI/Migrations/20230216204207_FixResultsTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixResultsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "answers",
+                table: "Results");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "answers",
+                table: "Results",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
