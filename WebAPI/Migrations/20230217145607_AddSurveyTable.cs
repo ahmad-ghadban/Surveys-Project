31bc6b5f24e0e587b6328c040f375615a9a6a69d
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-05 01:35:29 -0500

    Create 20230217145607_AddSurveyTable.cs

---
+++ WebAPI/Migrations/20230217145607_AddSurveyTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSurveyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Surveys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    questions = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    participants = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    respondents = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    deadline = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    published = table.Column<bool>(type: "bit", nullable: false),
+                    isPublic = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Surveys", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Surveys");
+        }
+    }
+}
