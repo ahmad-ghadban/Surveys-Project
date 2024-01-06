c96077528636a6ec19432f9668f3b96c5866052c
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-06 00:25:19 -0500

    Create 20230217213412_RenameDateSurvey.cs

---
+++ WebAPI/Migrations/20230217213412_RenameDateSurvey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDateSurvey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deadline",
+                table: "Surveys");
+
+            migrationBuilder.AddColumn<int>(
+                name: "deadline",
+                table: "Surveys",
+                type: "int",
+                nullable: false,
+                defaultValue: 4832283600);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deadline",
+                table: "Surveys");
+        }
+    }
+}
