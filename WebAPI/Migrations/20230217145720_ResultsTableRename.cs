81b5f42b9128bf5cbc02a3e9a30de1e693bb37c1
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-06 00:25:13 -0500

    Create 20230217145720_ResultsTableRename.cs

---
+++ WebAPI/Migrations/20230217145720_ResultsTableRename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResultsTableRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "questions",
+                table: "Results",
+                newName: "responses");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "responses",
+                table: "Results",
+                newName: "questions");
+        }
+    }
+}
