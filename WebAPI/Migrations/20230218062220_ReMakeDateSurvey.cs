d75390690d9ff369ae2950bff4cf180e68616d2c
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-07 14:14:49 -0500

    Create 20230218062220_ReMakeDateSurvey.cs

---
+++ WebAPI/Migrations/20230218062220_ReMakeDateSurvey.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReMakeDateSurvey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "deadline",
+                table: "Surveys",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "deadline",
+                table: "Surveys",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
