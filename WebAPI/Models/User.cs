0a30608a89efc97bfd069ae1ad80e2b94b37e271
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-08 00:42:38 -0500

    Create User.cs

---
+++ WebAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+
+        public string Completed { get; set; }
+        public string Access { get; set; }
+        public string Active { get; set; }
+    }
+}
