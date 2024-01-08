1192d5031a2a9858bc8e2387a6d06a56fb092d5b
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-08 00:42:32 -0500

    Create Result.cs

---
+++ WebAPI/Models/Result.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+
+        public int userID{ get; set; }
+
+        public int surveyID{ get; set; }
+
+        public string responses { get; set; }
+    }
+}
