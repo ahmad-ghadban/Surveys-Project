f06fbca1059160c40dc84b603ae1c75507861407
Ahmad Ghadban <50277734+ahmad-ghadban@users.noreply.github.com>
2024-01-08 00:42:33 -0500

    Create Survey.cs

---
+++ WebAPI/Models/Survey.cs
@@ -0,0 +1,23 @@
+﻿namespace WebAPI.Models
+{
+    public class Survey
+    {
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public string questions { get; set; }
+
+        public string participants { get; set; }
+
+        public string respondents { get; set; }
+
+        public string deadline { get; set; }
+
+        public bool published { get; set; }
+
+        public bool isPublic { get; set; }
+    }
+}
